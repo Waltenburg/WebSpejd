9bb829d0666f583424ef652b46fe472d1d7bc9b5
HashingOwl <rothowa@gmail.com>
2023-03-29 10:30:49 +0200

    Implementeret bedre håndtering af filer og errors
    Ny funktion getFile der håndterer læsning af filer og fejl

--- Deployed/server.js
+++ Deployed/server.js
@@ -2,10 +2,36 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var http = require("http");
 var fs = require("fs");
+var process = require("process");
+process.chdir(__dirname);
 var hostname = '127.0.0.1';
 var port = 3000;
-var server = http.createServer(createServer);
-server.listen(port, hostname, function () { return console.log("Server is now listening at http://".concat(hostname, ":").concat(port)); });
+var server = http.createServer(function (req, res) {
+    var headers = req.headers, method = req.method, url = req.url;
+    var body;
+    var reqData = null;
+    req.on("error", function (error) { return console.log(error); })
+        .on('data', function (chunk) { body.push(chunk); })
+        .on('end', function () {
+        if (body != undefined)
+            reqData = Buffer.concat(body).toString();
+        console.log("Request type: ".concat(method, ", URL: ").concat(url, ", Data: ").concat(reqData));
+    });
+    res.setHeader('Content-Type', 'text/html');
+    getFile('index.html', function (file) { return res.end(file); }, function () { res.writeHead(500); res.end(); });
+}).listen(port, hostname, function () { return console.log("Server is now listening at http://".concat(hostname, ":").concat(port)); });
+var getFile = function (path, succesCallback, failCallback) {
+    fs.readFile(path, function (error, data) {
+        if (isError(error)) {
+            console.log("error reading file " + path);
+            if (failCallback != null)
+                failCallback();
+        }
+        else
+            succesCallback(data);
+        function isError(error) { return !(!error); }
+    });
+};
 var Lob = (function () {
     function Lob(navn, antalPoster) {
         this.navn = navn;
@@ -33,20 +59,6 @@ function saveAsCSV(data, fileName, path) {
 function loadCSV(fileName, path) {
     var data = fs.readFileSync("".concat(path, "/").concat(fileName, ".csv")).toString().split("\n").map(function (e) { return e.split(","); });
     return data;
-}
-function createServer(req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    fs.readFile('index.html', sendResponse);
-    function sendResponse(error, data) {
-        if (isError(error))
-            console.log("error");
-        else
-            res.write(data);
-        res.end();
-        function isError(error) {
-            return !(!error);
-        }
-    }
 }
 var data = [
     ['1', '2', '3'],
--- Development/server.ts
+++ Development/server.ts
@@ -1,15 +1,52 @@
 import * as http from 'http'
 import * as fs from 'fs'
 
+import process = require('process');
+import { buffer } from 'stream/consumers';
+import { match } from 'assert';
+process.chdir(__dirname)
+
 const hostname = '127.0.0.1';
 const port = 3000;
-const server: http.Server = http.createServer(createServer);
-server.listen(port, hostname, () => console.log(`Server is now listening at http://${hostname}:${port}`))
 
+const server: http.Server = http.createServer((req: http.IncomingMessage, res: http.ServerResponse): void => {
+    const {headers, method, url} = req
+
+    let body: Buffer[]
+    let reqData: string = null
+    req.on("error", error => console.log(error))
+    .on('data', chunk => {body.push(chunk)})
+    .on('end', () => {
+        if(body != undefined)
+            reqData = Buffer.concat(body).toString()
+        console.log(`Request type: ${method}, URL: ${url}, Data: ${reqData}`)
+    })
+
+    res.setHeader('Content-Type', 'text/html')
+    getFile('index.html', file => res.end(file), () => {res.writeHead(500); res.end()})
+}).listen(port, hostname, () => console.log(`Server is now listening at http://${hostname}:${port}`))
+
+let getFile = (path: string, succesCallback: singleParamCallback<Buffer>, failCallback?: singleParamCallback<void>): void => {
+    fs.readFile(path, (error: NodeJS.ErrnoException | null, data: Buffer): void => {
+        if (isError(error)){
+            console.log("error reading file " + path)
+            if(failCallback != null)
+                failCallback()
+        }
+        else
+            succesCallback(data)
+        function isError(error: NodeJS.ErrnoException | null): error is NodeJS.ErrnoException { return !(!error) }
+    })
+    //JEG TROR AT PROBLEMET ER AT FS.READFILE BRUGER CALLBACK. DERFOR NÅR file AT BLIVE RETURNERET FØR DEN ER BLEVET DEFINERET I CALLBACKEN
+    //LØSNINGEN MÅ VÆRE AT LAVE DEN FUNKTION OM SÅ DEN OGSÅ KALDER EN CALLBACK NÅR FILEN ER FUNDET OG EVENTUELLE FEJL ER HÅNDTERET
+}
+interface singleParamCallback<T>{
+    (file: T): void
+}
 class Lob {
     navn: string;
-    antalPoster: bigint
-    constructor(navn: string, antalPoster: bigint) {
+    antalPoster: number
+    constructor(navn: string, antalPoster: number) {
         this.navn = navn
         this.antalPoster = antalPoster
     }
@@ -27,8 +64,8 @@ class Post{
         this.lukketid = lukketid;
         this.omvejLukketid = omvejLukketid;
     }
-
 }
+
 function saveAsCSV (data: string[][], fileName: string, path: string){
     const csv: string = data.map(row => row.join(',')).join('\n')
     fs.writeFile(`${path}/${fileName}.csv`, csv, (err) => {
@@ -37,24 +74,8 @@ function saveAsCSV (data: string[][], fileName: string, path: string){
     })
 }
 function loadCSV(fileName: string, path: string) : string[][]{
-    var data = fs.readFileSync(`${path}/${fileName}.csv`).toString().split("\n").map(e => e.split(","));
+var data = fs.readFileSync(`${path}/${fileName}.csv`).toString().split("\n").map(e => e.split(","));
     return data
-}
-function createServer(req: http.IncomingMessage, res: http.ServerResponse): void {
-    res.writeHead(200, { 'Content-Type': 'text/html' })
-    fs.readFile('index.html', sendResponse)
-    
-    function sendResponse(error: NodeJS.ErrnoException | null, data: Buffer) {
-        if (isError(error))
-            console.log("error")
-        else
-            res.write(data)
-        res.end()
-
-        function isError(error: NodeJS.ErrnoException | null): error is NodeJS.ErrnoException {
-            return !(!error)
-        }
-    }
 }
 
 const data: string[][] = [
