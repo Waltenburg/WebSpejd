39bc8b07cb31f0ad1455b93619eb5b0cec8d2227
Roald Waltenburg <78874031+Waltenburg@users.noreply.github.com>
2025-01-08 22:42:47 +0100

    Added login test

--- src/tests/startServer.spec.ts
+++ src/tests/startServer.spec.ts
@@ -8,42 +8,54 @@ const PORT = 43210;
 let serverProcess: ChildProcessWithoutNullStreams;
 const reqTimedOut = new Error("Request timed out");
 
-describe("Requests to server", () => {
-    before((done) => {
-        // console.log("Starting server");
-        const command = `node ${pathToServer} --database ${pathToDatabase} --port ${PORT} --assets ${pathToAssets}`;
-        // const command = 'echo %cd%';
-        // console.log(command);
+const startServer = (done: Mocha.Done) => {
+    serverProcess = spawn('node', [pathToServer, '--database', pathToDatabase, '--port', `${PORT}`, '--assets', pathToAssets], { stdio: 'pipe' });
 
-        serverProcess = spawn('node', [pathToServer, '--database', pathToDatabase, '--port', `${PORT}`, '--assets', pathToAssets], { stdio: 'pipe' });
-
-        serverProcess.on('error', (err) => {
-            console.error("Error starting server:", err);
-            done(err);
-            assert.fail(err.message);
-        });
-
-        serverProcess.stdout?.on('data', (data) => {
-            const output:string = data.toString();
-            if (output.includes("Server is now listening")) {
-                done();
-            }
-            else if(output.includes("Program exiting")) {
-                // done("Server exited");
-                assert.fail("Server exited");
-            }
-        });
-
-        serverProcess.stderr?.on('data', (data) => {
-            console.error("stderr:", data.toString());
-            done(data);
-            assert.fail(data.toString());
-        });
+    serverProcess.on('error', (err) => {
+        console.error("Error starting server:", err);
+        done(err);
+        assert.fail(err.message);
     });
-    after(() => {
-        console.log("Stopping server");
-        serverProcess.kill();
-    })
+
+    serverProcess.stdout?.on('data', (data) => {
+        const output:string = data.toString();
+        if (output.includes("Server is now listening")) {
+            done();
+        }
+        else if(output.includes("Program exiting")) {
+            // done("Server exited");
+            assert.fail("Server exited");
+        }
+    });
+
+    serverProcess.stderr?.on('data', (data) => {
+        console.error("stderr:", data.toString());
+        done(data);
+        assert.fail(data.toString());
+    });
+}
+
+const stopServer = () => {
+    console.log("Stopping server");
+    serverProcess.kill();
+}
+
+const request = (adress: String, headers: Headers, assertion: (response: Response) => void, done: Mocha.Done) => {
+    const url = `http://localhost:${PORT}/` + adress
+    fetch(url, {
+        headers: headers,
+        signal: AbortSignal.timeout(200)
+    }).then((response) => {
+        assertion(response);
+        done();
+    }).catch(() => {
+        done(reqTimedOut);
+    });
+}
+
+describe("Requests to server", () => {
+    before(startServer);
+    after(stopServer);
 
     it("Should respond with 200", (done) => {
         fetch(`http://localhost:${PORT}`).then((response) => {
@@ -54,7 +66,7 @@ describe("Requests to server", () => {
         });
     });
 
-    it("Should still respond with 200", (done) => {
+    it("Should get file", (done) => {
         // fetch(`http://localhost:${PORT}/css/loginStyle.css`).then((response) => {
         fetch(`http://localhost:${PORT}/assets/css/loginStyle.css`, { signal: AbortSignal.timeout(200) }).then((response) => {
             assert.equal(response.status, 200);
@@ -63,4 +75,23 @@ describe("Requests to server", () => {
             done(reqTimedOut);
         });
     });
+
+    it("Should login", (done) => {
+        request("login", new Headers({
+            "id": "test",
+            "password": "egernsamlernødder"
+        }), (response) => {
+            assert.equal(response.status, 200);
+        },
+        done);
+    })
+    it("Should not login", (done) => {
+        request("login", new Headers({
+            "id": "test",
+            "password": "NotTheRightPassword"
+        }), (response) => {
+            assert.oneOf(response.status, [401, 404]);
+        },
+        done);
+    })
 })
