396b8c824f18a46b63aa17c1a8e4a38c9c81058b
Roald Waltenburg <78874031+Waltenburg@users.noreply.github.com>
2025-12-21 11:21:33 +0100

    Comment out old database file

--- src/database.ts
+++ src/database.ts
@@ -1,5 +1,5 @@
-import { Database, PatrolUpdate, CheckinType, Location } from "./database/generic";
-import { JsonDatabase } from "./database/jsonDatabase";
-import { DatabaseWrapper } from "./database/wrapper";
+// import { Database, PatrolUpdate, PatrolUpdateType, Location } from "./database/generic";
+// import { JsonDatabase } from "./database/jsonDatabase";
+// import { DatabaseWrapper } from "./database/wrapper";
 
-export { DatabaseWrapper, JsonDatabase, PatrolUpdate as Checkin, CheckinType, Location as Post, Database };
+// export { DatabaseWrapper, JsonDatabase, PatrolUpdate as Checkin, PatrolUpdateType as CheckinType, Location as Post, Database };
