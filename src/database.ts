845a275e3d055326706e6a56fd978b8a0f33efc9
Roald Waltenburg <78874031+Waltenburg@users.noreply.github.com>
2025-12-22 22:02:36 +0100

    Replace database.ts with databaseBarrel.ts
    Removed the old database.ts barrel file and added databaseBarrel.ts to re-export updated service and database modules. This improves clarity and organization of database-related exports.

--- src/database.ts
+++
@@ -1,5 +0,0 @@
-// import { Database, PatrolUpdate, PatrolUpdateType, Location } from "./database/generic";
-// import { JsonDatabase } from "./database/jsonDatabase";
-// import { DatabaseWrapper } from "./database/wrapper";
-
-// export { DatabaseWrapper, JsonDatabase, PatrolUpdate as Checkin, PatrolUpdateType as CheckinType, Location as Post, Database };
